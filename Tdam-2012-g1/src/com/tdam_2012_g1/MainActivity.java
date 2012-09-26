9dd5442fc0beb53b2a0b07548fbcc9427b5bcfd7
maximilianoescribano <maximilianoescribano@9b7007f8-784f-dd99-4726-a7e931bb2d3c>
2012-09-26 01:24:32 +0000

    Clase Main con activity de contactos

--- Tdam-2012-g1/src/com/tdam_2012_g1/MainActivity.java
+++ Tdam-2012-g1/src/com/tdam_2012_g1/MainActivity.java
@@ -1,20 +1,64 @@
 package com.tdam_2012_g1;
 
+
+
+
+import android.view.View.OnClickListener;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
+	Intent intent;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        
+        ImageButton contactos = (ImageButton)findViewById(R.id.imgbtn_contactos);
+        
+        contactos.setOnClickListener(this);
+                
     }
+    
+    @Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.imgbtn_contactos:
+			intent = new Intent().setClass(this, Contacts.class);
+			startActivity(intent);
+			break;
+		}
+	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_profile:
+			Intent intent = new Intent(this, Settings_activity.class);
+			startActivity(intent);
+			break;
+		}
+		return true;
+	}
+    
+    
 }
+
+	
+
+
+
